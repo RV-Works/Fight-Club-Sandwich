b421d05d119c393b69b65698606dc8860614a0cf
StefandePutter <Stefandeputter@gmail.com>
2025-11-13 14:34:46 +0100

    added controls

---
+++ sandwich/Assets/Scripts/Stefan/InputManager.cs
@@ -0,0 +1,64 @@
+//using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public delegate void buttonDelegate();
+    public delegate void vector2Delegate(Vector2 moveInput);
+
+    [SerializeField] private PlayerInput playerInput;
+    private InputActionAsset inputAsset;
+    private InputActionMap player;
+
+    private Controls m_playerInput;
+    private float m_lookInput = 0f;
+    private Vector2 m_moveInput = new Vector2();
+
+    public buttonDelegate dashEvent;
+    public vector2Delegate moveEvent;
+
+    private void Awake()
+    {
+
+        inputAsset = playerInput.actions;
+
+        player = inputAsset.FindActionMap("Player");
+    }
+
+    void Start()
+    {
+        // movement
+        player.FindAction("Move").performed += OnMove;
+        player.FindAction("Move").canceled += OnMove;
+
+        player.FindAction("Dash").performed += OnDash;
+
+        player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // values
+        player.FindAction("Move").performed -= OnMove;
+        player.FindAction("Move").canceled -= OnMove;
+
+        // buttons
+        player.FindAction("Dash").performed -= OnDash;
+
+        
+        player.Disable();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        m_moveInput = context.ReadValue<Vector2>();
+        moveEvent?.Invoke(m_moveInput);
+    }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        // if any are connected invoke
+        dashEvent?.Invoke();
+    }
+}
---
+++ sandwich/Assets/Scripts/Stefan/PlayerMovement.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [SerializeField] private float m_speed = 6;
+    [SerializeField] private float m_turnSpeed = 5f;
+    [SerializeField] private float m_dashForce = 10f;
+    [SerializeField] private float m_dashCooldown = 2f;
+    [SerializeField] private InputManager m_inputManager;
+
+    private Vector3 moveInput;
+
+    private Rigidbody m_rigidBody;
+    private bool m_sprinting = false;
+    private Vector3 m_velocity;
+    private float m_currentDashCooldown = 0f;
+
+    private void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        // subscribing to callbacks
+        m_inputManager.moveEvent += SetMove;
+        m_inputManager.dashEvent += Dash;
+    }
+
+    private void OnDisable()
+    {
+        // unsubscribing from callbacks
+        m_inputManager.moveEvent -= SetMove;
+        m_inputManager.dashEvent -= Dash;
+    }
+
+    private void FixedUpdate()
+    {
+        if (m_inputManager == null)
+            return;
+
+        // moving
+        if (moveInput.x != 0f || moveInput.z != 0f)
+        {
+            // move character
+            float speed = m_sprinting ? m_speed * 2.5f : m_speed;
+             m_rigidBody.AddForce(moveInput * speed);
+
+            // rotate towards movement
+            Quaternion targetRotation = Quaternion.LookRotation(moveInput);
+            //m_rigidBody.MoveRotation(targetRotation);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, m_turnSpeed * Time.deltaTime);
+        }
+        else
+        // not moving
+        {
+            m_sprinting = false;
+        }
+
+        if (m_currentDashCooldown > 0f)
+            m_currentDashCooldown -= Time.deltaTime;
+    }
+
+    private void SetMove(Vector2 setMoveInput)
+    {
+        // set input
+        moveInput = new Vector3(setMoveInput.x, 0, setMoveInput.y).normalized;
+    }
+
+    private void Dash()
+    {
+        // dash
+        if (m_currentDashCooldown <= 0f)
+        {
+            m_rigidBody.AddRelativeForce(Vector3.forward * m_dashForce);
+            m_currentDashCooldown = m_dashCooldown;
+        }
+    }
+}
